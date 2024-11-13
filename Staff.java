19ee3cc7f9c865818c8769288244e65d5c25448c
Cashew2022 <catherinekoniec@gmail.com>
2024-11-13 09:31:18 -0400

    Addition of class for employee and payroll in, and constructor for staff

---
+++ Staff.java
@@ -0,0 +1,14 @@
+// Staff class for managing employee information and payroll
+public class Staff extends Person {
+    private int yearsOfService;
+    private double salary;
+
+    /*Constructor for Staff
+     * Calculates salary based on years of service using private implementation
+     * Base salary is $50000 with $500 step increase per year
+     */
+    public Staff(String name, String address, int yearsOfService) {
+        super(name, address);
+        this.yearsOfService = yearsOfService;
+        this.salary = 50000 + (yearsOfService * 500);
+    }
