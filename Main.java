922bf83a4d53f92b175476b7cae7e097627041a5
Cashew2022 <catherinekoniec@gmail.com>
2024-11-13 09:19:07 -0400

    main application and entry point

--- Main.java
+++ Main.java
@@ -12,3 +12,21 @@ public class Main {
     // Lists to store students and staff
     private ArrayList<Student> students = new ArrayList<>();
     private ArrayList<Staff> staff = new ArrayList<>();
+
+    // Application entry point: Creates instance of Main and starts the program
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    // Main program loop: Displays option dialog for user selection between Student, Staff, or Finish
+    public void run() {
+        while (true) {
+            String[] options = {"Student", "Staff", "Finish"};
+            int choice = JOptionPane.showOptionDialog(null,
+                    "Select Student or Staff.",
+                    "Accounting App",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    options,
+                    options[0]);
