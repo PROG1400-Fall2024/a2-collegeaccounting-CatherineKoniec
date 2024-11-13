c8e197eb8b37bc6056c962b101b98ad4373496f2
Cashew2022 <catherinekoniec@gmail.com>
2024-11-13 09:27:01 -0400

    Addition of student class and constructor for student

--- Student.java
+++ Student.java
@@ -1,3 +1,18 @@
-public class Student {
+/* Student class representing college students
+ * Handles student-specific attributes and fee calculations
+ * Implements private attributes for year and fees
+ * Inherits name and address from Person class
+ */
+public class Student extends Person {
+    private int year;
+    private double fees;
 
-}
+    /* Constructor for Student
+     * Calculates fees based on year level using private implementation
+     * Base fee is $3000 with $100 increment per year
+     */
+    public Student(String name, String address, int year) {
+        super(name, address);
+        this.year = year;
+        this.fees = 3000 + ((year - 1) * 100);
+    }
