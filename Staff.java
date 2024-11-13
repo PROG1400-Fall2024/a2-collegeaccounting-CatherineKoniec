8ef1b3ef7b6aa84bc3f7c495de530cb8b2b623f7
Cashew2022 <catherinekoniec@gmail.com>
2024-11-13 09:29:53 -0400

    Addition of getter methods for persons name and address. Removing code from staff file that was added in error- should have been in person.java

--- Staff.java
+++ Staff.java
@@ -1,23 +1,4 @@
-// Base class for all persons in the system
-public abstract class Person {
-    private String name;
-    private String address;
 
-    // Constructor for Person
-    // Initializing person with name and address
-    public Person(String name, String address) {
-        this.name = name;
-        this.address = address;
-    }
-    // Getter methods:
-    // Retrieves the person's name
-    public String getName() {
-        return name;
-    }
-    // Retrieves the person's address
-    public String getAddress() {
-        return address;
-    }
     // Abstract toString method to be implemented by subclasses
     @Override
     public abstract String toString();
