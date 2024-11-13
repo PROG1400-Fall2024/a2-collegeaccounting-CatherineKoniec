7729263b86bbf013f3f93f7d71701107fc8f80e7
Cashew2022 <catherinekoniec@gmail.com>
2024-11-13 09:29:06 -0400

    Addition of class for all persons and constructor for person

--- Person.java
+++ Person.java
@@ -1,2 +1,11 @@
-public class Person {
-}
+// Base class for all persons in the system
+public abstract class Person {
+    private String name;
+    private String address;
+
+    // Constructor for Person
+    // Initializing person with name and address
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
